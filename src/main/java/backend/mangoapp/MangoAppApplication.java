21fecec6b8b09b001995d71e569168c253d898e2
Timofii <timofejberezanskij@gmail.com>
2023-12-11 15:57:02 +0200

    1-st commit

---
+++ src/main/java/backend/mangoapp/MangoAppApplication.java
@@ -0,0 +1,13 @@
+package backend.mangoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangoAppApplication.class, args);
+    }
+
+}
