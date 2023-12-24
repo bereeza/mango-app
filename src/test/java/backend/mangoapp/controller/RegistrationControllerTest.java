9bb24d0d90334acc61a79ca4b269a66d232d48d1
Timofii <timofejberezanskij@gmail.com>
2023-12-24 15:16:52 +0200

    edit controller test

--- src/test/java/backend/mangoapp/controller/RegistrationControllerTest.java
+++ src/test/java/backend/mangoapp/controller/RegistrationControllerTest.java
@@ -1,13 +1,17 @@
 package backend.mangoapp.controller;
 
 import backend.mangoapp.entity.User;
+import backend.mangoapp.service.userService.UserService;
 import org.junit.jupiter.api.Assertions;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,15 +24,25 @@ public class RegistrationControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @MockBean
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User("bob@gmail.com", "12345", "@bob");
+    }
+
     @Test
     public void testSignUp() {
-        User user = new User("bob@gmail.com", "12345", "@bob");
+        when(userService.add(any(User.class))).thenReturn(user);
+
         ResponseEntity<User> response = restTemplate.postForEntity("/", user, User.class);
 
         Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
 
-        assertThat(Objects.requireNonNull(response.getBody()).getId()).isGreaterThan(0);
-        Assertions.assertEquals("bob@gmail.com", response.getBody().getEmail());
+        Assertions.assertEquals("bob@gmail.com", Objects.requireNonNull(response.getBody()).getEmail());
         Assertions.assertEquals("@bob", response.getBody().getNickname());
     }
 
