a48ff168f9a9c52443d5968fc097f00b59828c9f
Timofii <timofejberezanskij@gmail.com>
2024-01-03 14:43:21 +0200

    entity service test

---
+++ src/test/java/backend/mangoapp/service/CommentServiceTest.java
@@ -0,0 +1,100 @@
+package backend.mangoapp.service;
+
+import backend.mangoapp.entity.Comment;
+import backend.mangoapp.entity.Post;
+import backend.mangoapp.entity.User;
+import backend.mangoapp.repository.CommentRepository;
+import backend.mangoapp.service.commentService.CommentService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CommentServiceTest {
+
+    @InjectMocks
+    private CommentService commentService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    private User user;
+    private Post post;
+    private Comment comment;
+
+    @BeforeEach
+    public void setup() {
+        user = new User("carl@gmail.com", "12345", "@carl");
+        post = new Post("Test description", Timestamp.valueOf(LocalDateTime.now()), user, List.of());
+        comment = new Comment("Test comment", Timestamp.valueOf(LocalDateTime.now()), user, post);
+    }
+
+    @Test
+    public void saveCommentTest() {
+        Comment savedComment = new Comment("Save this comment", Timestamp.valueOf(LocalDateTime.now()), user, post);
+        commentService.add(savedComment);
+        verify(commentRepository, times(1)).save(savedComment);
+        assertThat(savedComment).isNotNull();
+        assertThat(savedComment.getUser()).isNotNull();
+        assertThat(savedComment.getPost()).isNotNull();
+    }
+
+    @Test
+    public void getCommentByIdTest() {
+        long id = comment.getId();
+
+        when(commentRepository.findById(id)).thenReturn(Optional.of(comment));
+        Optional<Comment> result = commentService.getById(id);
+        verify(commentRepository, times(1)).findById(id);
+
+        assertThat(result).isNotEmpty();
+        assertThat(result.get().getUser()).isNotNull();
+        assertThat(result.get().getPost()).isNotNull();
+
+        assertThat(result.get()).isEqualTo(comment);
+    }
+
+    @Test
+    public void getAllCommentsTest() {
+        Comment newComment = new Comment("Test comment", Timestamp.valueOf(LocalDateTime.now()), user, post);
+        commentService.add(newComment);
+        verify(commentRepository, times(1)).save(newComment);
+
+        assertThat(newComment).isNotNull();
+        assertThat(newComment.getUser()).isNotNull();
+        assertThat(newComment.getPost()).isNotNull();
+
+        List<Comment> comments = List.of(comment, newComment);
+        assertThat(commentService.getAll()).isNotNull();
+        assertThat(comments.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void deleteCommentByIdTest() {
+        long id = comment.getId();
+
+        commentService.deleteById(id);
+        verify(commentRepository, times(1)).deleteById(id);
+        assertThat(commentService.getById(id)).isEmpty();
+    }
+
+    @Test
+    public void deleteCommentEntityTest() {
+        long id = comment.getId();
+
+        commentService.delete(comment);
+        verify(commentRepository, times(1)).delete(comment);
+        assertThat(commentService.getById(id)).isEmpty();
+    }
+}
---
+++ src/test/java/backend/mangoapp/service/PostServiceTest.java
@@ -0,0 +1,91 @@
+package backend.mangoapp.service;
+
+import backend.mangoapp.entity.Post;
+import backend.mangoapp.entity.User;
+import backend.mangoapp.repository.PostRepository;
+import backend.mangoapp.service.postService.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    @InjectMocks
+    private PostService postService;
+
+    @Mock
+    private PostRepository postRepository;
+
+    private Post post;
+    private User user;
+
+    @BeforeEach
+    public void setup() {
+        user = new User("carl@gmail.com", "12345", "@carl");
+        post = new Post("Test description", Timestamp.valueOf(LocalDateTime.now()), user, List.of());
+    }
+
+    @Test
+    public void savePostTest() {
+        Post savedPost = new Post("Save this post", Timestamp.valueOf(LocalDateTime.now()), user, List.of());
+        postService.add(savedPost);
+        verify(postRepository, times(1)).save(savedPost);
+        assertThat(savedPost).isNotNull();
+        assertThat(savedPost.getUser()).isNotNull();
+    }
+
+    @Test
+    public void getPostByIdTest() {
+        long id = post.getId();
+
+        when(postRepository.findById(id)).thenReturn(Optional.of(post));
+        Optional<Post> result = postService.getById(id);
+        verify(postRepository, times(1)).findById(id);
+
+        assertThat(result).isNotEmpty();
+        assertThat(result.get().getUser()).isNotNull();
+        assertThat(result.get()).isEqualTo(post);
+    }
+
+    @Test
+    public void getAllPostsTest() {
+        Post newPost = new Post("New post!", Timestamp.valueOf(LocalDateTime.now()), user, List.of());
+        postService.add(newPost);
+        verify(postRepository, times(1)).save(newPost);
+        assertThat(newPost).isNotNull();
+        assertThat(newPost.getUser()).isNotNull();
+
+        List<Post> posts = List.of(post, newPost);
+        assertThat(postService.getAll()).isNotNull();
+        assertThat(posts.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void deletePostByIdTest() {
+        long id = post.getId();
+
+        postService.deleteById(id);
+        verify(postRepository, times(1)).deleteById(id);
+        assertThat(postService.getById(id)).isEmpty();
+    }
+
+    @Test
+    public void deletePostEntityTest() {
+        long id = post.getId();
+        postService.delete(post);
+        verify(postRepository, times(1)).delete(post);
+        assertThat(postService.getById(id)).isEmpty();
+    }
+}
