a178b0a7c28ca9891b353459ea920cad6c99576c
Timofii <timofejberezanskij@gmail.com>
2024-01-08 15:06:59 +0200

    security config

--- src/main/java/backend/mangoapp/configuration/Config.java
+++ src/main/java/backend/mangoapp/configuration/Config.java
@@ -1,14 +1,50 @@
 package backend.mangoapp.configuration;
 
+import backend.mangoapp.entity.User;
 import backend.mangoapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import java.util.Optional;
 
 @Configuration
-public class Config {
+public class Config extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/signin", "/").permitAll()
+                .anyRequest().authenticated()
+                .and().httpBasic();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> {
+            Optional<User> userOptional = userRepository.findUserByEmail(email);
+            return userOptional.map(user -> org.springframework.security.core.userdetails.User
+                            .withUsername(user.getEmail())
+                            .password(user.getPassword())
+                            .roles()
+                            .build())
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with email."));
+        };
+    }
 }
