a465a07e86dcf20a2d7530e10bd66024f0e0d2d9
Timofii <timofejberezanskij@gmail.com>
2024-12-06 17:25:57 +0200

    edit redis model

---
+++ mango-gateway-service/src/main/java/com/mango/mangogatewayservice/dto/user/UserRedisInfo.java
@@ -0,0 +1,20 @@
+package com.mango.mangogatewayservice.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRedisInfo implements Serializable {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+}
