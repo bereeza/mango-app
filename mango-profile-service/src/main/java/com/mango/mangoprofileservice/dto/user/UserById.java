0c28cbbfbafea623133a5e073e4d345a2b29708d
Timofii <timofejberezanskij@gmail.com>
2024-12-12 17:35:46 +0200

    setup bucket privacy

---
+++ mango-profile-service/src/main/java/com/mango/mangoprofileservice/dto/user/UserById.java
@@ -0,0 +1,23 @@
+package com.mango.mangoprofileservice.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserById implements Serializable {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private String cv;
+    private String about;
+    private String reputation;
+    private String link;
+}
