c76a9f73354db2010fe130a92310c5fc3acac993
Timofii <timofejberezanskij@gmail.com>
2024-12-15 16:55:50 +0200

    fix

--- mango-post-service/src/main/java/com/mango/postservice/entity/User.java
+++ mango-post-service/src/main/java/com/mango/postservice/entity/User.java
@@ -5,11 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
-
-import java.util.List;
 
 @Data
 @Builder
@@ -47,7 +44,4 @@ public class User {
 
     @Column(value = "link")
     private String link;
-
-    @Transient
-    private List<Post> posts;
 }
